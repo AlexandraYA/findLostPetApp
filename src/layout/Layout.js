81686015ab5c841b4a333fd870a74129022ae445
Alexandra Abrosimova <madam.abrosimova@yandex.ru>
2020-01-29 06:39:52 +0300

    Layout creating started

---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Layout = (props) => {
+    return (
+        <div>
+            <h1>Категории</h1>
+            <p>Какая-то еще инфа</p>
+        </div>
+    );
+};
+
+export default Layout;
